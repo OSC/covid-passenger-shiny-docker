495ef6128b81f0d8d821f74f8a61a20208a61971
Jeff Ohrstrom <johrstrom@osc.edu>
2021-09-29 12:41:16 -0400

    add comments for focalmap users

--- user-setup.sh
+++ user-setup.sh
@@ -2,12 +2,14 @@
 
 set -xe
 
-## Add users (covidsim, covidsimdev, covid, coviddev)
+## Add users (covidsim, covidsimdev, covid, coviddev, focalmap, focalmapdev)
 
 # uid=33929(covid) gid=6697(PAS1788) groups=6697(PAS1788)
 # uid=33930(coviddev) gid=6697(PAS1788) groups=6697(PAS1788)
 # uid=33024(covidsim) gid=6493(PAS1694) groups=6493(PAS1694),6602(PZS1008),6603(accesscovidsim)
 # uid=33031(covidsimdev) gid=6493(PAS1694) groups=6493(PAS1694),6604(accesscovidsimdev),6602(PZS1008)
+# uid=36970(focalmap) gid=2925(PZS0523) groups=2925(PZS0523),7175(PDE0001),6393(duo)
+# uid=36971(focalmapdev) gid=2925(PZS0523) groups=2925(PZS0523),6393(duo),7175(PDE0001)
 
 groupadd -g 6697 PAS1788
 groupadd -g 6493 PAS1694
